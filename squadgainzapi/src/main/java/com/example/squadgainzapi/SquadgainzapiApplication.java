70ad37c996ef1bfe78a8547e8e41936ca516a117
freddypoly99@gmail.com <freddypoly99@gmail.com>
2024-09-08 12:45:04 +0530

    New repo

---
+++ squadgainzapi/src/main/java/com/example/squadgainzapi/SquadgainzapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.squadgainzapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadgainzapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadgainzapiApplication.class, args);
+	}
+
+}
---
+++ squadgainzapi/src/test/java/com/example/squadgainzapi/SquadgainzapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.squadgainzapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquadgainzapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
